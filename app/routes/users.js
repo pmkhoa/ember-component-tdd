09e29b9ab83e8d6298c9e2c181fd2355d8b713ac
Khoa Pham <khoa.pham@me.com>
2015-07-17 14:54:43 -0700

    Add model to users route and hash it to name users.

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return Ember.RSVP.hash({
+      users: this.store.find("user")
+    });
+  },
+
+  setupController: function(controller, models) {
+    controller.setProperties(models);
+  }
+
 });
