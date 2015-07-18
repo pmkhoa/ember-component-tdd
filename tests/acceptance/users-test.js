0a0266131ddea6703e509f26557bbb51c2dd8960
Khoa Pham <khoa.pham@me.com>
2015-07-17 18:22:15 -0700

    Initial acceptance user test

---
+++ tests/acceptance/users-test.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'test-component-example/tests/helpers/start-app';
+
+var application;
+
+module('Acceptance | users', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('visiting /users', function(assert) {
+  visit('/users');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/users');
+  });
+});
+
+test('should list all customer', function(assert) {
+  visit('/users');
+
+  andThen(function() {
+    assert.equal(find(".user").length, 0);
+  });
+});
